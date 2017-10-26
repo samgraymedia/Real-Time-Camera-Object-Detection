b45cab648005d254e1049604ec339a61db5834f0
Samuel Gray <sam.gray22@gmail.com>
2017-10-26 20:47:44 +0100

    commit

--- Real Time Camera Object Detection/ViewController.swift
+++ Real Time Camera Object Detection/ViewController.swift
@@ -24,7 +24,6 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         super.viewDidLoad()
         
         // here is where we start up the camera
-        // for more details visit: https://www.letsbuildthatapp.com/course_video?id=1252
         let captureSession = AVCaptureSession()
         captureSession.sessionPreset = .photo
         
@@ -60,9 +59,6 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         //        print("Camera was able to capture a frame:", Date())
         
         guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
-        
-        // !!!Important
-        // make sure to go download the models at https://developer.apple.com/machine-learning/ scroll to the bottom
         guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else { return }
         let request = VNCoreMLRequest(model: model) { (finishedReq, err) in
             
